402e967e3b787753b0d177808d3947675a4fe118
MansuVerma <verma.mansu04@gmail.com>
2026-02-11 18:46:12 +0530

    Push

---
+++ chaining.py
@@ -0,0 +1,5 @@
+letter='''Dear <name>
+You are selected for <subject> viva
+on the date <date>.
+so be on time.'''
+print(letter.replace("<name>" ,"Mansu").replace("<subject>","chemistry").replace("<date>","5th september"))
