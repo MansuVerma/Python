1fc4650a5d9f89646f5ddfce0461c96b549d409b
MansuVerma <verma.mansu04@gmail.com>
2026-02-21 10:31:53 +0530

    Push

---
+++ forloop_else.py
@@ -0,0 +1,11 @@
+# list=[2,4,5,7,7,58]
+# for i in list:
+#     print(i)
+# else:
+#     print("done..")
+tuple=(2,3,5,5,5,7,8,9,11,23)
+print(tuple)
+for i in tuple:
+    if i==5:
+     continue
+    print("elements of tuple",i)
