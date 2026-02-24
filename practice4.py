76d98cd4b43f8fb56424dcccd35121ca1730e62f
MansuVerma <verma.mansu04@gmail.com>
2026-02-24 22:31:58 +0530

    Push

---
+++ practice4.py
@@ -0,0 +1,7 @@
+word=input("Enter the word to be deleted: ")
+
+with open("file1.txt","r")as f:
+    content=f.read()
+contentnew=content.replace(word,"#####")
+with open("file1.txt","w") as f:
+    f.write(contentnew)
